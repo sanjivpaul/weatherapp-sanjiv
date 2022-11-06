cde867c365bd0763c7bc878057ed593691722187
Risav Singh <risavsingh7462@gmail.com>
2022-11-06 21:34:17 +0530

    weather-app-v0.1.0🚀

---
+++ README.md
@@ -0,0 +1,7 @@
+# **This is Weather app by Sanjiv Paul**
+
+### _`Author: Sanjiv Paul`_
+*This is mini project 02 of edureka full stack Web Development Course* 
+## This is weather app made with using `HTML`, `CSS` & `JavaScript`.
+
+## ![weatherapp](weatherApp.png)
---
+++ script.js
@@ -0,0 +1,52 @@
+let weather = {
+  apikey: "e662dc7845f453912610516dd2fa3dd7",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apikey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    const { country } = data.sys;
+    const sunrise = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+    const sunset = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+    // console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "weather in " + name;
+    document.querySelector(".icon").src =
+      "http://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    document.querySelector(".country").innerText = "Country:" + country;
+    document.querySelector(".sunrise").innerText = "Sunrise:" + sunrise;
+    document.querySelector(".sunset").innerText = "Sunset:" + sunset;
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+document.querySelector(".search-button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
